7616a9189976f8c2ae26bd863dde9b4bff06489a
orginalsk01 <sipssman@gmail.com>
2023-06-07 20:13:22 +0200

    Add sign up

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,30 @@
+const User = require("../models/user.model");
+
 function getSignup(req, res) {
   res.render("customer/auth/signup");
 }
 
-function getLogin(req, res) {}
+async function signup(req, res) {
+  const user = new User(
+    req.body.email,
+    req.body.password,
+    req.body.fullName,
+    req.body.street,
+    req.body.postal,
+    req.body.city
+  );
+
+  await user.signUp();
+
+  res.redirect("/login");
+}
+
+function getLogin(req, res) {
+  res.render("customer/auth/login");
+}
 
 module.exports = {
   getSignup: getSignup,
+  signup: signup,
   getLogin: getLogin,
 };
