57c967907cc1ca2ead8e8fe6c73a4b0dbcd2112c
orginalsk01 <sipssman@gmail.com>
2023-06-22 20:34:19 +0200

    Bug fix or cart badge on mobile

--- public/scripts/cart-management.js
+++ public/scripts/cart-management.js
@@ -2,7 +2,7 @@ const addToCartButtonElement = document.querySelector(
   "#product-details button"
 );
 
-const cartBadgeElement = document.querySelector(".nav-items .badge");
+const cartBadgeElements = document.querySelectorAll(".nav-items .badge");
 
 async function addToCart(event) {
   const productId = addToCartButtonElement.dataset.productid;
@@ -34,7 +34,9 @@ async function addToCart(event) {
 
   const newTotalQuantity = responseData.newTotalItems;
 
-  cartBadgeElement.textContent = newTotalQuantity;
+  for (const cartBadgeElement of cartBadgeElements) {
+    cartBadgeElement.textContent = newTotalQuantity;
+  }
 }
 
 addToCartButtonElement.addEventListener("click", addToCart);
