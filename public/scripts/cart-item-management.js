392d2fee30c42e94248b3e1f65f3e7c32b7a1b8b
orginalsk01 <sipssman@gmail.com>
2023-06-22 20:32:45 +0200

    Include cart item management js to update cart for users

--- public/scripts/cart-item-management.js
+++ public/scripts/cart-item-management.js
@@ -36,6 +36,22 @@ async function updateCartItem(event) {
   }
 
   const responseData = await response.json();
+
+  console.log(responseData);
+
+  const cartItemTotalPriceElement =
+    form.parentElement.querySelector(".cart-item-price");
+  cartItemTotalPriceElement.textContent =
+    responseData.updatedCartData.updatedItemPrice.toFixed(2);
+
+  const cartTotalPriceElement = document.getElementById("cart-total-price");
+  cartTotalPriceElement.textContent =
+    responseData.updatedCartData.newTotalPrice.toFixed(2);
+
+  const cartBadgeElements = document.querySelector(".nav-items .badge");
+  for (const cartBadge of cartBadgeElements) {
+    cartBadge.textContent = responseData.updatedCartData.newTotalQuantity;
+  }
 }
 
 for (const formElement of cartItemUpdateFormElements) {
